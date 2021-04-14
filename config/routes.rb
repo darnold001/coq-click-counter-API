0d34982b38d22082fdd5c5cf79c8a798546638dd
Devin Arnold <dbarnold321@gmail.com>
2021-04-14 13:11:40 -0700

    removed comments in routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/sign_up', to: 'users#create'
       resources :clicks
-          # get 'get_clicks', to: 'clicks#index'
-          # post 'add_click', to: 'clicks#create'
       resources :images
-          # post '/', to: 'auth#create'
-          # get '/current_user', to: 'auth#show'
       resources :users
           post '/login', to: 'auth#create'
           get '/current_user', to: 'auth#show'
